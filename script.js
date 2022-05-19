648f47497b4a6b8c2e0dfbdff6f8a17d9095e949
Maneesh Cheema <maneeshcheema17@gmail.com>
2022-05-20 01:56:46 +0530

    comments added

--- script.js
+++ script.js
@@ -1,74 +1,73 @@
 const screen = document.getElementsByClassName("calc-screen")[0];
-let buffer = "0";
-let previousOperator = undefined;
+let buffer = "0"; // screen content
+let previousOperator = undefined; // operator before a number
 let total = 0;
+
+/**
+ * refresh content of calculator screen
+ */
+function rerender(buffer) {
+  screen.innerText = buffer;
+}
 
 function getOperation(button) {
   if (isNaN(button)) {
-    handleOperator(button);
+    handleOperator(button); // input is a operator
   } else {
-    handleNumber(button);
+    handleNumber(button); // input is a number
   }
-  rerender(buffer);
-}
-
-function handleNumber(button) {
-  if (buffer === "0") {
-    buffer = button;
-  } else {
-    buffer += button;
-  }
+  rerender(buffer); // refresh screen after each input
 }
 
 function handleOperator(button) {
-  if (button === "C") {
-    buffer = "0";
-  } else if (button === "←") {
-    if (buffer.length === 1) {
+  if (button === "C") { // clear screen content
+    buffer = "0"; 
+  } else if (button === "←") { // backspace screen content
+    if (buffer.length === 1) { // only one character on screen
       buffer = "0";
     } else {
       buffer = buffer.substring(0, buffer.length - 1);
     }
-  } else if(button === "="){
-    if(previousOperator === undefined) return;
-    doMath();
-    previousOperator = undefined;
-    buffer = "" + total;
-    total = 0;
-  }
-  else{
-      handleMath(button);
+  } else if (button === "=") {
+    if (previousOperator === undefined) return; // any operation require one operator
+    doMath(); 
+    previousOperator = undefined; // operation done, disard operator
+    buffer = "" + total; // update screen content
+    total = 0; // calculation done, reset
+  } else {
+    handleMath(button);
   }
 }
 
-function handleMath(button){
-    if(buffer === "0") return;
-    if(total === 0){
-        total = parseInt(buffer);
-    }
-    else{
-        doMath();
-    }
-    previousOperator = button;
-    buffer = "0";
+function handleNumber(button) {
+  if (buffer === "0") { // single digit number
+    buffer = button;
+  } else {
+    buffer += button; // multi digit number
+  }
+}
+function handleMath(button) {
+  if (buffer === "0") return; // no operand
+  if (total === 0) { // first operand
+    total = parseInt(buffer);
+  } else {
+    doMath();
+  }
+  previousOperator = button; // required for next operation
+  buffer = "0"; // clear screen content
 }
 
 function doMath() {
-    intBuffer = parseInt(buffer);
-    if(previousOperator === "+") total += intBuffer;
-    else if(previousOperator === "-") total -= intBuffer;
-    else if(previousOperator === "*") total *= intBuffer;
-    else if(previousOperator === "/") total /= intBuffer;
-}
-
-function rerender(buffer) {
-  screen.innerText = buffer;
+  intBuffer = parseInt(buffer); // operations only on integers
+  if (previousOperator === "+") total += intBuffer;
+  else if (previousOperator === "-") total -= intBuffer;
+  else if (previousOperator === "*") total *= intBuffer;
+  else if (previousOperator === "/") total /= intBuffer;
 }
 
 function main() {
   document.querySelector(".calc").addEventListener("click", function (event) {
     getOperation(event.target.innerText);
-    console.log(buffer);
   });
 }
 
